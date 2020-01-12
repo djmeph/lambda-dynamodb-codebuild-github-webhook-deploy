a34791fca932ccffe4a4aee95379728e74b725df
Norman Witte III <norm.witte@gmail.com>
2020-01-12 02:14:32 -0500

    Add some guardrails

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const dynamodb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 
 exports.handler = async (event) => {
     const body = JSON.parse(event.body);
+    const response = {
+        statusCode: 200,
+        body: null
+    }
+    
+    if (!body || !body.ref || !body.repository && body.repository.full_name) {
+        return response;
+    }
     
     const data = await dynamodb.get({
         TableName: process.env.DYNAMO_TABLE,
@@ -15,22 +23,22 @@ exports.handler = async (event) => {
         }
     }).promise();
     
+    if (!data && data.Item) {
+        return response;
+    }
+    
     const sig = `sha1=${crypto.createHmac('sha1', data.Item.githubWebhookSecret).update(event.body).digest('hex')}`;
     if (sig !== event.headers['X-Hub-Signature']) throw new Error('Unauthorized Signature');
     
-    if (body.ref === data.Item.ref) {
-        console.log({
-            ref: body.ref,
-            repo: body.repository.full_name,
-            codeBuild: data.Item.codeBuildProject
-        });
-        await codebuild.startBuild({
-            projectName: data.Item.codeBuildProject
-        }).promise();
-    }
-    const response = {
-        statusCode: 200,
-        body: null,
-    };
+    console.log({
+        ref: body.ref,
+        repo: body.repository.full_name,
+        codeBuild: data.Item.codeBuildProject
+    });
+    
+    await codebuild.startBuild({
+        projectName: data.Item.codeBuildProject
+    }).promise();
+
     return response;
 };
